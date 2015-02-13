6cf57757b3fbd013fe0089a239340803d4a76e92
icy-wolf <nicole.lawrence93@gmail.com>
2015-02-12 19:49:49 -0500

    Completed Dry Synatra, TC 359 Project 6

--- app.rb
+++ app.rb
@@ -1,9 +1,12 @@
 # app.rb
 require 'sinatra'
+require 'sinatra/base'
+require 'sinatra/content_for'
 
 class MyWebApp < Sinatra::Base
+  helpers Sinatra::ContentFor
   get '/' do
   last_modified Time.now
-  send_file "views/index#{ rand(3) + 1 }.html"
+  erb :"index#{ rand(3) + 1 }"
 end
 end
